0b2f64061feb426270b83b1b841be9a2ca376f4f
Jakub Królik <01142179@pw.edu.pl>
2021-03-20 16:00:33 +0100

    bardzo wazny commit

---
+++ StringCalculator/StringCalculator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CodeKata
+{
+    public class StringCalculator
+    {
+        public int add(string number) 
+        {
+            if(number=="") return 0;
+            string extraSplitter=",";
+            if(number.Contains("//"))
+            {
+                var toextraString=number.Split(new string[] { "//" }, StringSplitOptions.None);
+                extraSplitter = toextraString[1][0].ToString();
+                number = toextraString[1].Substring(1);
+            }           
+            string[] numbers = number.Split(new string[] { "\r\n", "\n",",",extraSplitter }, StringSplitOptions.None);
+            int result = 0;
+            foreach(string item in numbers)
+            {
+                if (item.Contains("-")) throw new ArgumentException();
+                if (item.Length<4) { result += Int32.Parse(item); } //item must be smaller than 1000          
+            }
+            return result;
+        }
+    }
+}
---
+++ StringCalculatorTests/StringCalculatorTests.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodeKata;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeKata.Tests
+{
+    [TestClass()]
+    public class StringCalculatorTests
+    {
+        StringCalculator stringCalculator = new StringCalculator();
+        [TestMethod()]
+        public void addTestEmptyString()
+        {            
+            string emptyString = "";
+            int result = stringCalculator.add(emptyString);
+            Assert.AreEqual(0, result);
+        }
+        [TestMethod()]
+        public void addTestSingleValue()
+        {
+            string string123 = "123";
+            int result = stringCalculator.add(string123);
+            Assert.AreEqual(123, result);
+        }
+        [TestMethod()]
+        public void addTestAddingTwoNumbers()
+        {
+            string stringTest = "123,456";
+            int result = stringCalculator.add(stringTest);
+            Assert.AreEqual(579, result);
+        }
+        [TestMethod()]
+        public void addTestAddingTwoNumbersNewLineSeparator()
+        {
+            string stringTest = "123\n456";
+            int result = stringCalculator.add(stringTest);
+            Assert.AreEqual(579, result);
+        }
+        [TestMethod()]
+        public void addTestAddingThreeNumbers()
+        {
+            string stringTest = "123\n456,21";
+            int result = stringCalculator.add(stringTest);
+            Assert.AreEqual(600, result);
+        }
+        [TestMethod(),ExpectedException(typeof(ArgumentException))]
+        public void addTestNegativeNumberException()
+        {
+            string stringTest = "123\n-456,21";
+            int result = stringCalculator.add(stringTest);
+        }
+        [TestMethod()]
+        public void addTestAddingNumberGraterThan1000()
+        {
+            string stringTest = "123\n456,21,1200";
+            int result = stringCalculator.add(stringTest);
+            Assert.AreEqual(600, result);
+        }
+        [TestMethod()]
+        public void addTestCustomSeparator()
+        {
+            string stringTest = "//a123\n456,21,1200a20";
+            int result = stringCalculator.add(stringTest);
+            Assert.AreEqual(620, result);
+        }
+
+    }
+}
